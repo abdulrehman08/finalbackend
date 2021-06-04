ffa9ed3b881fec8188caf4d0addfe581a8f57fe5
Abdul Rehman <nodebazaar@gmail.com>
2021-06-04 14:01:01 +0500

    env set

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express')
 var app = express();
 const path=require('path');
+const env=require('dotenv');
+
 app.use(express.static(path.join(__dirname,"public")));
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, `/public/index.html`));
 });
-app.listen(5000,console.log("frontend application started at 5000"))
+env.config({path:'/config.env'});
+
+app.listen(process.env.PORT||5000,console.log("application started at 5000"))
